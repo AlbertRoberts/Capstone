3f54e9a21338b3194c7dadf0e6e16f590ee9b122
Martinbeaballa <martin.balla.22@cnu.edu>
2026-02-17 17:40:41 -0500

    basic town layout

--- frontend/src/scenes/townscene.ts
+++ frontend/src/scenes/townscene.ts
@@ -18,6 +18,200 @@ export default class townscene extends Phaser.Scene {
 
 	editorCreate(): void {
 
+		// rectangle_1
+		const rectangle_1 = this.add.rectangle(632, 371, 128, 128);
+		rectangle_1.scaleX = 10.180187948025909;
+		rectangle_1.scaleY = 5.833727976370516;
+		rectangle_1.isFilled = true;
+		rectangle_1.fillColor = 3108670;
+
+		// rectangle
+		const rectangle = this.add.rectangle(644, 628, 128, 128);
+		rectangle.scaleX = 0.37167610523713657;
+		rectangle.scaleY = 1.4867068094771634;
+		rectangle.isFilled = true;
+		rectangle.fillColor = 9408399;
+
+		// rectangle_2
+		const rectangle_2 = this.add.rectangle(644, 551, 128, 128);
+		rectangle_2.scaleX = 0.32718513673271565;
+		rectangle_2.scaleY = 7.251572595017952;
+		rectangle_2.angle = -90;
+		rectangle_2.isFilled = true;
+		rectangle_2.fillColor = 9408399;
+
+		// rectangle_3
+		const rectangle_3 = this.add.rectangle(201, 361, 128, 128);
+		rectangle_3.scaleX = 0.37328872859877876;
+		rectangle_3.scaleY = 3.245378044183737;
+		rectangle_3.isFilled = true;
+		rectangle_3.fillColor = 9408399;
+
+		// rectangle_4
+		const rectangle_4 = this.add.rectangle(1085, 361, 128, 128);
+		rectangle_4.scaleX = 0.37328872859877876;
+		rectangle_4.scaleY = 3.245378044183737;
+		rectangle_4.isFilled = true;
+		rectangle_4.fillColor = 9408399;
+
+		// rectangle_5
+		const rectangle_5 = this.add.rectangle(647, 176, 128, 128);
+		rectangle_5.scaleX = 0.32718513673271565;
+		rectangle_5.scaleY = 7.251572595017952;
+		rectangle_5.angle = -90;
+		rectangle_5.isFilled = true;
+		rectangle_5.fillColor = 9408399;
+
+		// rectangle_7
+		const rectangle_7 = this.add.rectangle(317, 363, 128, 128);
+		rectangle_7.scaleY = 2.466942142176058;
+		rectangle_7.isFilled = true;
+		rectangle_7.fillColor = 4662308;
+
+		// rectangle_8
+		const rectangle_8 = this.add.rectangle(305, 66, 128, 128);
+		rectangle_8.isFilled = true;
+		rectangle_8.fillColor = 4662308;
+
+		// rectangle_9
+		const rectangle_9 = this.add.rectangle(481, 66, 128, 128);
+		rectangle_9.isFilled = true;
+		rectangle_9.fillColor = 4662308;
+
+		// rectangle_10
+		const rectangle_10 = this.add.rectangle(668, 66, 128, 128);
+		rectangle_10.isFilled = true;
+		rectangle_10.fillColor = 4662308;
+
+		// rectangle_11
+		const rectangle_11 = this.add.rectangle(852, 66, 128, 128);
+		rectangle_11.isFilled = true;
+		rectangle_11.fillColor = 4662308;
+
+		// rectangle_12
+		const rectangle_12 = this.add.rectangle(1019, 66, 128, 128);
+		rectangle_12.isFilled = true;
+		rectangle_12.fillColor = 4662308;
+
+		// rectangle_13
+		const rectangle_13 = this.add.rectangle(1209, 224, 128, 128);
+		rectangle_13.isFilled = true;
+		rectangle_13.fillColor = 4662308;
+
+		// rectangle_14
+		const rectangle_14 = this.add.rectangle(1212, 372, 128, 128);
+		rectangle_14.isFilled = true;
+		rectangle_14.fillColor = 4662308;
+
+		// rectangle_15
+		const rectangle_15 = this.add.rectangle(1209, 527, 128, 128);
+		rectangle_15.isFilled = true;
+		rectangle_15.fillColor = 4662308;
+
+		// rectangle_16
+		const rectangle_16 = this.add.rectangle(85, 545, 128, 128);
+		rectangle_16.isFilled = true;
+		rectangle_16.fillColor = 4662308;
+
+		// rectangle_17
+		const rectangle_17 = this.add.rectangle(82, 378, 128, 128);
+		rectangle_17.isFilled = true;
+		rectangle_17.fillColor = 4662308;
+
+		// rectangle_18
+		const rectangle_18 = this.add.rectangle(79, 218, 128, 128);
+		rectangle_18.isFilled = true;
+		rectangle_18.fillColor = 4662308;
+
+		// rectangle_19
+		const rectangle_19 = this.add.rectangle(977, 450, 128, 128);
+		rectangle_19.isFilled = true;
+		rectangle_19.fillColor = 4662308;
+
+		// rectangle_20
+		const rectangle_20 = this.add.rectangle(977, 268, 128, 128);
+		rectangle_20.isFilled = true;
+		rectangle_20.fillColor = 4662308;
+
+		// TOwn_hall
+		const tOwn_hall = this.add.rectangle(647, 369, 128, 128);
+		tOwn_hall.scaleX = 2.001800422483393;
+		tOwn_hall.scaleY = 1.1617588487187138;
+		tOwn_hall.isFilled = true;
+		tOwn_hall.fillColor = 4662308;
+
+		// rectangle_22
+		const rectangle_22 = this.add.rectangle(799, 646, 128, 128);
+		rectangle_22.scaleX = 1.4865908809477275;
+		rectangle_22.scaleY = 0.8796780398859774;
+		rectangle_22.isFilled = true;
+		rectangle_22.fillColor = 4662308;
+
+		// rectangle_23
+		const rectangle_23 = this.add.rectangle(389, 643, 128, 128);
+		rectangle_23.scaleX = 2.777556167090955;
+		rectangle_23.scaleY = 0.696841503238748;
+		rectangle_23.isFilled = true;
+		rectangle_23.fillColor = 4662308;
+
+		// rectangle_24
+		const rectangle_24 = this.add.rectangle(466, 364, 128, 128);
+		rectangle_24.scaleX = 0.37328872859877876;
+		rectangle_24.scaleY = 3.245378044183737;
+		rectangle_24.angle = 11;
+		rectangle_24.isFilled = true;
+		rectangle_24.fillColor = 9408399;
+
+		// rectangle_25
+		const rectangle_25 = this.add.rectangle(831, 365, 128, 128);
+		rectangle_25.scaleX = 0.37328872859877876;
+		rectangle_25.scaleY = 3.245378044183737;
+		rectangle_25.angle = -11;
+		rectangle_25.isFilled = true;
+		rectangle_25.fillColor = 9408399;
+
+		// rectangle_6
+		const rectangle_6 = this.add.rectangle(1102, 652, 128, 128);
+		rectangle_6.scaleX = 2.288062440058767;
+		rectangle_6.scaleY = 0.416422309655215;
+		rectangle_6.isFilled = true;
+		rectangle_6.fillColor = 9489506;
+
+		// text_1
+		const text_1 = this.add.text(611, 345, "", {});
+		text_1.text = "Town Hall";
+		text_1.setStyle({  });
+
+		// text
+		const text = this.add.text(277, 344, "", {});
+		text.text = "School";
+		text.setStyle({  });
+
+		// text_2
+		const text_2 = this.add.text(950, 259, "", {});
+		text_2.text = "Clinic";
+		text_2.setStyle({  });
+
+		// text_3
+		const text_3 = this.add.text(942, 441, "", {});
+		text_3.text = "Cafe";
+		text_3.setStyle({  });
+
+		// text_4
+		const text_4 = this.add.text(360, 643, "", {});
+		text_4.text = "Tavern";
+		text_4.setStyle({  });
+
+		// text_5
+		const text_5 = this.add.text(762, 631, "", {});
+		text_5.text = "Market";
+		text_5.setStyle({  });
+
+		// text_6
+		const text_6 = this.add.text(1013, 641, "", {});
+		text_6.text = "Park\n";
+		text_6.setStyle({  });
+
 		this.events.emit("scene-awake");
 	}
 
